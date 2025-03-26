6ebc83b8b03c1fca205d347f3bf776a74102f27a
bakerwm <wangm08@yeah.net>
2025-03-26 16:55:34 +0800

    create data/ dir, save target.bed files

--- config.sh
+++ config.sh
@@ -23,14 +23,14 @@
 export N_CPU=12
 
 # Genome configuration
-export GENOME_BUILD="GRCm38"
+export GENOME_BUILD="GRCh38"
 export RELEASE=102
-export BOWTIE2_IDX="/data/biodata/genome_db/GRCm38/Ensembl/bowtie2_index/GRCm38"
+export BOWTIE2_IDX="genome/${GENOME_BUILD}" 
 
 # Analysis parameters
-export TARGET_BED="igh_regions.mouse.bed"  # Required, BED3 format
-export FLANKING_LEFT=2000000         # 2 Mbp around IgH genes
-export FLANKING_RIGHT=0              # 2 Mbp around IgH genes
+export TARGET_BED="data/igh_regions.human.bed"  # Required, BED3 format
+export FLANKING_LEFT=0         # 2 Mbp around IgH genes
+export FLANKING_RIGHT=2000000              # 2 Mbp around IgH genes
 export MIN_COPY=10                   # Minimum copy number of repeat sequences
 export MIN_LENGTH=13                 # Minimum length of repeat sequences
 # END: Required VARIABLES                                                      #
